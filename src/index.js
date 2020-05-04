cf304782f11a065d00a95dcd08f59c92f498f4b5
Jonathan Fielding <j.fielding@me.com>
2020-05-04 21:42:55 +0100

    fix: get the example working

--- src/index.js
+++ src/index.js
@@ -11,6 +11,21 @@ import useNodeRunner from 'use-node-runner';
 import Terminal from 'spectacle-output-terminal';
 import { Deck, FlexBox, Slide } from 'spectacle';
 
+import createTheme from 'spectacle/lib/themes/default';
+
+const theme = createTheme(
+  {
+    primary: 'white',
+    secondary: '#1F2022',
+    tertiary: '#03A9FC',
+    quaternary: '#CECECE',
+  },
+  {
+    primary: 'Montserrat',
+    secondary: 'Helvetica',
+  }
+);
+
 const Presentation = () => {
   const [code, setCode] = useState(``);
   const { setRunnerCode, response, isRunning } = useNodeRunner(
@@ -35,7 +50,7 @@ const Presentation = () => {
   };
 
   return (
-    <Deck transitionEffect="fade">
+    <Deck theme={theme} transitionEffect="fade">
       <Slide>
         <SpectacleCodemirror
           value={code}
